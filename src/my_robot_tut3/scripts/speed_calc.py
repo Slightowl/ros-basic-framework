7f9120368cd33b44fb1154c9f2d35764fa39624d
Slightowl <slightowler7@gmail.com>
2021-05-11 18:58:16 +0100

    comment update

--- src/my_robot_tut3/scripts/speed_calc.py
+++ src/my_robot_tut3/scripts/speed_calc.py
@@ -8,14 +8,29 @@ wheel_radius = rospy.get_param("/wheel_radius")
 
 
 def calc_speed(rpm, publisher):
+    """calc_speed summary.
+    Args:
+        rpm (float): Takes in rpm or revolutions per minute.
+        publisher (type): .
+    Returns:
+        float: Calculation of speed to publisher
+    """
     wheel_radius = rospy.get_param("/wheel_radius")
     speed = rpm.data * 2 * 3.1459 / 60 * wheel_radius # speed in m/s
     publisher.publish(speed)
 
 def create_subscriber(pub):
+    """create_subscriber summary.
+    Args:
+        pub (object): sets Subscriber parameters.
+    """
     rospy.Subscriber("rpm", Float32, calc_speed, (pub))
 
 def speed_pub():
+    """speed_pub summary.
+    Returns:
+        object: Publisher object containing label, value and size.
+    """
     pub = rospy.Publisher("rspeed", Float32, queue_size=10)
     return pub
 
@@ -23,6 +38,5 @@ if __name__ == '__main__':
     rospy.init_node("speed_calc_sub_node")
     pub = speed_pub()
     # pub = rospy.Publisher("speed", Float32 ,queue_size=10) # different way of doing def speed_pub
-
     create_subscriber(pub)
     rospy.spin()
